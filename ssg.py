954a9a6b9adf935c18ecddbb16728f7b13c9aa76
ramya6016 <ramya6016@gmail.com>
2022-02-18 17:09:01 +0530

    The site class

--- ssg.py
+++ ssg.py
@@ -2,9 +2,8 @@ import typer
 from ssg.site import Site
 def main(self,source="content",dest="dist"):
     config={'config':source,'dest':dest}
-    Site_instance=Site(**config)
-    Site_instance.build()
-typer.run(main())
+    Site(**config).build()
+typer.run(main)
 
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -5,10 +5,10 @@ class Site:
         self.source=Path(source)
         self.dest=Path(dest)
     def create_dir(self,path):
-        directory=self.dest+'/'+relative_to(self.source)
-        mkdir(directory,parents=True,exist_ok=True)
+        directory=self.dest/path.relative_to(self.source)
+        os.mkdir(directory,parents=True,exist_ok=True)
     def build(self):
-        mkdir(self.dest,parents=True,exist_ok=True)
+        os.mkdir(self.dest, parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if(os.path.isdir(path)):
                 self.create_dir(path)
