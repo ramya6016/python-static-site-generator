a5fc8307ab1e7bf40d3d8c96d4c0d7f9fbaa67a4
ramya6016 <ramya6016@gmail.com>
2022-02-18 18:58:58 +0530

    The parser class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,37 +2,30 @@ from typing import List
 from pathlib import Path
 import shutil
 class Parser:
-    extensions=List[str]
+    extensions=List[str]=[]
+
     def valid_extension(self,extension):
-        if(extension in self.extensions):
-            return True
-        else:
-            return False
-    def parse(self,path,source,dest):
-        path=Path()
-        source=Path()
-        dest=Path()
+        return (extension in self.extensions)
+
+    def parse(self,path=Path,source=Path,dest=Path):
         raise NotImplementedError
 
     def read(self,path):
-        with open(path) as file:
+        with open(path,"r") as file:
             return file.read()
 
     def write(self,path,dest,content,ext=".html"):
-        full_path=self.dest/path.with_suffix(ext).name
-        with open(full_path) as file:
+        full_path=dest/path.with_suffix(ext).name
+        with open(full_path,"w") as file:
             file.write(content)
+
     def copy(self,path,source,dest):
         shutil.copy2(path,dest/path.relative_to(source))
 
-    class ResourceParser(Parser):
-        extensions=[".jpg",".png",".gif",".css",".html"]
-
-        def parse(self, path, source, dest):
-            path = Path()
-            source = Path()
-            dest = Path()
-            raise NotImplementedError
+class ResourceParser(Parser):
+    extensions=[".jpg",".png",".gif",".css",".html"]
+    def parse(self, path, source, dest):
+        self.copy(path,source,dest)
 
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -17,16 +17,16 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif(path.is_file()):
-                Site.run_parser(path)
+                self.run_parser(path)
 
     def load_parser(self,extension):
         for parser in self.parsers:
             if(parser.valid_extension(extension)):
                 return parser
     def run_parser(self,path):
-        parser=Site.load_parser(path.suffix)
+        parser=self.load_parser(path.suffix)
         if(parser is not None):
-            parser.parse(path,parser.source,parser.dest)
+            parser.parse(path,self.source,self.dest)
         else:
             print("Not implemented")
 
