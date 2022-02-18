650c79b925c8f9ecd884b5db9de73756b133d649
ramya6016 <ramya6016@gmail.com>
2022-02-18 18:47:53 +0530

    The parser class

---
+++ ssg/parsers.py
@@ -0,0 +1,38 @@
+from typing import List
+from pathlib import Path
+import shutil
+class Parser:
+    extensions=List[str]
+    def valid_extension(self,extension):
+        if(extension in self.extensions):
+            return True
+        else:
+            return False
+    def parse(self,path,source,dest):
+        path=Path()
+        source=Path()
+        dest=Path()
+        raise NotImplementedError
+
+    def read(self,path):
+        with open(path) as file:
+            return file.read()
+
+    def write(self,path,dest,content,ext=".html"):
+        full_path=self.dest/path.with_suffix(ext).name
+        with open(full_path) as file:
+            file.write(content)
+    def copy(self,path,source,dest):
+        shutil.copy2(path,dest/path.relative_to(source))
+
+    class ResourceParser(Parser):
+        extensions=[".jpg",".png",".gif",".css",".html"]
+
+        def parse(self, path, source, dest):
+            path = Path()
+            source = Path()
+            dest = Path()
+            raise NotImplementedError
+
+
+
